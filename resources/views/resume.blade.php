3a2d211a3e20772426846e2b62dafa98bb50e96c
chilwinlim14 <74442256+chilwinlim14@users.noreply.github.com>
2022-09-30 01:44:38 +0700

    first commit

---
+++ resources/views/resume.blade.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Chilwin [CV]</title>
+
+        <!-- Fonts -->
+        <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+
+        <!-- Styles -->
+        <style>
+            /*! normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css */html{line-height:1.15;-webkit-text-size-adjust:100%}body{margin:0}a{background-color:transparent}[hidden]{display:none}html{font-family:system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;line-height:1.5}*,:after,:before{box-sizing:border-box;border:0 solid #e2e8f0}a{color:inherit;text-decoration:inherit}svg,video{display:block;vertical-align:middle}video{max-width:100%;height:auto}.bg-white{--bg-opacity:1;background-color:#fff;background-color:rgba(255,255,255,var(--bg-opacity))}.bg-gray-100{--bg-opacity:1;background-color:#f7fafc;background-color:rgba(247,250,252,var(--bg-opacity))}.border-gray-200{--border-opacity:1;border-color:#edf2f7;border-color:rgba(237,242,247,var(--border-opacity))}.border-t{border-top-width:1px}.flex{display:flex}.grid{display:grid}.hidden{display:none}.items-center{align-items:center}.justify-center{justify-content:center}.font-semibold{font-weight:600}.h-5{height:1.25rem}.h-8{height:2rem}.h-16{height:4rem}.text-sm{font-size:.875rem}.text-lg{font-size:1.125rem}.leading-7{line-height:1.75rem}.mx-auto{margin-left:auto;margin-right:auto}.ml-1{margin-left:.25rem}.mt-2{margin-top:.5rem}.mr-2{margin-right:.5rem}.ml-2{margin-left:.5rem}.mt-4{margin-top:1rem}.ml-4{margin-left:1rem}.mt-8{margin-top:2rem}.ml-12{margin-left:3rem}.-mt-px{margin-top:-1px}.max-w-6xl{max-width:72rem}.min-h-screen{min-height:100vh}.overflow-hidden{overflow:hidden}.p-6{padding:1.5rem}.py-4{padding-top:1rem;padding-bottom:1rem}.px-6{padding-left:1.5rem;padding-right:1.5rem}.pt-8{padding-top:2rem}.fixed{position:fixed}.relative{position:relative}.top-0{top:0}.right-0{right:0}.shadow{box-shadow:0 1px 3px 0 rgba(0,0,0,.1),0 1px 2px 0 rgba(0,0,0,.06)}.text-center{text-align:center}.text-gray-200{--text-opacity:1;color:#edf2f7;color:rgba(237,242,247,var(--text-opacity))}.text-gray-300{--text-opacity:1;color:#e2e8f0;color:rgba(226,232,240,var(--text-opacity))}.text-gray-400{--text-opacity:1;color:#cbd5e0;color:rgba(203,213,224,var(--text-opacity))}.text-gray-500{--text-opacity:1;color:#a0aec0;color:rgba(160,174,192,var(--text-opacity))}.text-gray-600{--text-opacity:1;color:#718096;color:rgba(113,128,150,var(--text-opacity))}.text-gray-700{--text-opacity:1;color:#4a5568;color:rgba(74,85,104,var(--text-opacity))}.text-gray-900{--text-opacity:1;color:#1a202c;color:rgba(26,32,44,var(--text-opacity))}.underline{text-decoration:underline}.antialiased{-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale}.w-5{width:1.25rem}.w-8{width:2rem}.w-auto{width:auto}.grid-cols-1{grid-template-columns:repeat(1,minmax(0,1fr))}@media (min-width:640px){.sm\:rounded-lg{border-radius:.5rem}.sm\:block{display:block}.sm\:items-center{align-items:center}.sm\:justify-start{justify-content:flex-start}.sm\:justify-between{justify-content:space-between}.sm\:h-20{height:5rem}.sm\:ml-0{margin-left:0}.sm\:px-6{padding-left:1.5rem;padding-right:1.5rem}.sm\:pt-0{padding-top:0}.sm\:text-left{text-align:left}.sm\:text-right{text-align:right}}@media (min-width:768px){.md\:border-t-0{border-top-width:0}.md\:border-l{border-left-width:1px}.md\:grid-cols-2{grid-template-columns:repeat(2,minmax(0,1fr))}}@media (min-width:1024px){.lg\:px-8{padding-left:2rem;padding-right:2rem}}@media (prefers-color-scheme:dark){.dark\:bg-gray-800{--bg-opacity:1;background-color:#2d3748;background-color:rgba(45,55,72,var(--bg-opacity))}.dark\:bg-gray-900{--bg-opacity:1;background-color:#1a202c;background-color:rgba(26,32,44,var(--bg-opacity))}.dark\:border-gray-700{--border-opacity:1;border-color:#4a5568;border-color:rgba(74,85,104,var(--border-opacity))}.dark\:text-white{--text-opacity:1;color:#fff;color:rgba(255,255,255,var(--text-opacity))}.dark\:text-gray-400{--text-opacity:1;color:#cbd5e0;color:rgba(203,213,224,var(--text-opacity))}}
+        </style>
+
+        <style>
+            body {
+                font-family: 'Poppins';
+                background-image: url("chilwin-background.png");
+                background-color:  rgba(0, 0, 0, 0.8);
+                background-repeat: no-repeat;
+            }
+        </style>
+        
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+    </head>
+    <body class="antialiased" style="font-family: Poppins">
+        
+        <nav class="navbar navbar-expand-lg bg-dark">
+            <div class="container">
+              <a class="navbar-brand" style="color:white" href="/home">Curiculum Vitae</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="nav nav-pills">
+                  <li class="nav-item">
+                    <a class="nav-link" style="color:white" href="/about">About</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link active" style="color:white" aria-current="page" href="/resume">Resume</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" style="color:white" href="/contact">Contact Me</a>
+                  </li>
+                </ul>
+
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link disabled">Disabled</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+          
+          <section id="resume" class="resume section-show" style="padding: 20px; background-color: rgba(0,0,0,0.6);">
+            <div class="container" style="margin-top: 1cm">
+              <div class="section-title" style="color: white">
+                <h2 style="font-size: 40px">Resume</h2>
+                <p>Check My Resume</p>
+              </div>
+              <div class="row" style="color: white">
+                <div class="col-lg-6">
+                  <h3 class="resume-title">Summary</h3>
+                  <div class="resume-item pb-0" style="color: white">
+                    <h4>Chilwin</h4>
+                    <p><em>Member of Public Relation Commission at Majelis Perwakilan Mahasiswa Universitas Pelita Harapan</em>
+                    </p>
+                    <p></p>
+                    <ul>
+                      <li>Medan, Indonesia</li>
+                      <li>(+62) 815-4132-9953</li>
+                      <li>chilwinlim12@gmail.com</li>
+                    </ul>
+                    <p></p>
+                  </div>
+                  <h3 class="resume-title">Education</h3>
+                  <div class="resume-item">
+                    <h4>Undergraduate, Information Systems</h4>
+                    <h5>2020 - 2024</h5>
+                    <p>
+                      <em>Universitas Pelita Harapan</em>
+                    </p>
+                  </div>
+                  <div class="resume-item">
+                    <h4>High School, Science</h4>
+                    <h5>2017 - 2020</h5>
+                    <p>
+                      <em>SMA Methodist-2 Medan</em>
+                    </p>
+                  </div>
+                </div>
+                <div class="col-lg-6">
+                  <h3 class="resume-title">Experiences</h3>
+                  <div class="resume-item">
+                    <h4>Member of Public Relation Commission</h4>
+                    <h5>2022 - Present</h5>
+                    <p>
+                      <em>Majelis Perwakilan Mahasiswa Universitas Pelita Harapan </em>
+                    </p>
+                    <p></p>
+                  </div>
+                  <div class="resume-item">
+                    <h4>Advisor of Basketball Club</h4>
+                    <h5>2022 - Present</h5>
+                    <p>
+                      <em>Basketball Club UPH Medan Campus</em>
+                    </p>
+                    <p></p>
+                  </div>
+                  <div class="resume-item">
+                    <h4>Member of Organization and Student Development Department</h4>
+                    <h5>2021 - 2022</h5>
+                    <p>
+                      <em>Badan Eksekutif Mahasiswa Universitas Pelita Harapan</em>
+                    </p>
+                    <p></p>
+                  </div>
+                  <div class="resume-item">
+                    <h4>Vice Coordinator of Basketball Club</h4>
+                    <h5>2021 - 2022</h5>
+                    <p>
+                      <em>Basketball Club UPH Medan Campus</em>
+                    </p>
+                    <p></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+    </body>
+</html>
